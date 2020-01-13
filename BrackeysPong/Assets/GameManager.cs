f0c8ddf121864463979cf9434d7a76822ed79494
Calvin James Newn <calvin.newn@mail.mcgill.ca>
2020-01-13 02:00:02 -0500

    Further development
    Added a score text to display on screen that updates as a player scores.

---
+++ BrackeysPong/Assets/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    static int player1Score = 0;
+    static int player2Score = 0;
+
+    public GUISkin skinScore; 
+
+    public static void IncrementScore(string wallName)
+    {
+        if (wallName == "Left Wall")
+        {
+            player2Score++;
+            Debug.Log("Player 2 scored");
+        }
+        else if (wallName == "Right Wall")
+        {
+            player1Score++;
+            Debug.Log("Player 1 scored");
+        }
+        Debug.Log("Current score: " + player1Score + " - " + player2Score) ;
+    }
+
+    void OnGUI()
+    {
+        GUI.skin = skinScore;
+        GUI.Label(new Rect(Screen.width / 2 - 150, 20, 100, 100), "" + player1Score);
+        GUI.Label(new Rect(Screen.width / 2 + 150, 20, 100, 100), "" + player2Score);
+    }
+}
---
+++ BrackeysPong/Assets/Scripts/PlayerScores.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PlayerScores : MonoBehaviour
+{
+    void OnTriggerEnter2D (Collider2D colliderInfo)
+    {
+        if (colliderInfo.tag == "Ball")
+        {
+            GameManager.IncrementScore(transform.name);
+        } 
+    }
+}
